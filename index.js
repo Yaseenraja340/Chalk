448356d31dce1ccde35eca8284374f3caebf08d4
rajayaseen <yaseenraja340@gmail.com>
2023-08-16 21:45:34 -0700

    chalk done with example

---
+++ index.js
@@ -0,0 +1,4 @@
+import chalk from 'chalk';
+console.log(chalk.red(`Raja yaseen:`));
+console.log(chalk.yellowBright(`I am 18 years old:`));
+console.log(chalk.green(`I am student of piaic`));
---
+++ index.ts
@@ -0,0 +1,8 @@
+import chalk from 'chalk';
+ 
+console.log(chalk.red(`Raj Yaseen:`));
+console.log(chalk.yellowBright(`I am 18 years old:`));
+console.log(chalk.green(`I am student of piaic`));
+
+
+
